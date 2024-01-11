baf5b8b0dc18f5661b2b50609801d2ae3c48c79d
Sebastião Fortes <sebastiaofortes4@gmail.com>
2024-01-11 09:17:35 -0300

    initial commit

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main()  {
+
+	fmt.Println("Iniciando app")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
+
+	fmt.Println("Iniciando servidor na porta 8080")
 
 	http.ListenAndServe(":8080", nil)
 }
